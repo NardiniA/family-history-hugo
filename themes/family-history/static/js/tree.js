1c3821e3f19169df952f585eccb225c35e06b603
NardiniA <techteacher.pershore@gmail.com>
2023-11-28 10:14:23 +0000

    Add Data Files and fix small bugs

--- themes/family-history/static/js/tree.js
+++ themes/family-history/static/js/tree.js
@@ -277,7 +277,8 @@ function init() {
 
   // key: slug, n: name, s: sex, m: mother, f: father, ux: wife, vir: husband
 
-  const data = JSON.parse(document.getElementById('myDiagramDiv').dataset.pages);
+  const diagramDiv = document.getElementById('myDiagramDiv');
+  const data = JSON.parse(diagramDiv.dataset.pages);
 
   const formattedData = data?.map(({ n, s, m, f, ux, vir }, index) => {
     mother = data.findIndex(el => el.key === m);
@@ -298,6 +299,8 @@ function init() {
 
     return result;
   });
+
+  const init = data.findIndex((d) => d.key == diagramDiv.dataset.init) || 0;
 
   // Since 2.2 you can also author concise templates with method chaining instead of GraphObject.make
   // For details, see https://gojs.net/latest/intro/buildingObjects.html
@@ -351,7 +354,7 @@ function init() {
       $(go.TextBlock,
         { textAlign: "center", maxSize: new go.Size(80, NaN), background: "rgba(255,255,255,0.5)" },
         new go.Binding("text", "n")),
-      { click: function (e, node) { window.open(`/${slugify(node.n) }`); } }
+      { click: function (e, node) { window.open(`/${slugify(formattedData[node.key]?.n)}`); } }
     ));
 
   // the representation of each label node -- nothing shows on a Marriage Link
@@ -379,59 +382,8 @@ function init() {
       $(go.Shape, { strokeWidth: 2.5, stroke: "#5d8cc1" /* blue */ })
     ));
 
-  setupDiagram(myDiagram, formattedData, "john-smith");
+  setupDiagram(myDiagram, formattedData, init);
 }
-
-// [
-//   { key: 0, n: "Aaron", s: "M", m: -10, f: -11, ux: 1 },
-//   { key: 1, n: "Alice", s: "F", m: -12, f: -13 },
-//   { key: 2, n: "Bob", s: "M", m: 1, f: 0, ux: 3 },
-//   { key: 3, n: "Barbara", s: "F" },
-//   { key: 4, n: "Bill", s: "M", m: 1, f: 0, ux: 5 },
-//   { key: 5, n: "Brooke", s: "F" },
-//   { key: 6, n: "Claire", s: "F", m: 1, f: 0 },
-//   { key: 7, n: "Carol", s: "F", m: 1, f: 0 },
-//   { key: 8, n: "Chloe", s: "F", m: 1, f: 0, vir: 9 },
-//   { key: 9, n: "Chris", s: "M" },
-//   { key: 10, n: "Ellie", s: "F", m: 3, f: 2 },
-//   { key: 11, n: "Dan", s: "M", m: 3, f: 2 },
-//   { key: 12, n: "Elizabeth", s: "F", vir: 13, a: ["J"] },
-//   { key: 13, n: "David", s: "M", m: 5, f: 4, a: ["B", "H"] },
-//   { key: 14, n: "Emma", s: "F", m: 5, f: 4 },
-//   { key: 15, n: "Evan", s: "M", m: 8, f: 9, a: ["F", "H"] },
-//   { key: 16, n: "Ethan", s: "M", m: 8, f: 9, a: ["D", "K"] },
-//   { key: 17, n: "Eve", s: "F", vir: 16, a: ["B", "F", "L"] },
-//   { key: 18, n: "Emily", s: "F", m: 8, f: 9 },
-//   { key: 19, n: "Fred", s: "M", m: 17, f: 16, a: ["B"] },
-//   { key: 20, n: "Faith", s: "F", m: 17, f: 16, a: ["L"] },
-//   { key: 21, n: "Felicia", s: "F", m: 12, f: 13, a: ["H"] },
-//   { key: 22, n: "Frank", s: "M", m: 12, f: 13, a: ["B", "H"] },
-
-//   // "Aaron"'s ancestors
-//   { key: -10, n: "Paternal Grandfather", s: "M", m: -33, f: -32, ux: -11, a: ["A", "S"] },
-//   { key: -11, n: "Paternal Grandmother", s: "F", a: ["E", "S"] },
-//   { key: -32, n: "Paternal Great", s: "M", ux: -33, a: ["F", "H", "S"] },
-//   { key: -33, n: "Paternal Great", s: "F", a: ["S"] },
-//   { key: -40, n: "Great Uncle", s: "M", m: -33, f: -32, a: ["F", "H", "S"] },
-//   { key: -41, n: "Great Aunt", s: "F", m: -33, f: -32, a: ["B", "I", "S"] },
-//   { key: "john-smith", n: "Uncle", s: "M", m: -11, f: -10, a: ["A", "S"] },
-
-//   // "Alice"'s ancestors
-//   { key: -12, n: "Maternal Grandfather", s: "M", ux: -13, a: ["D", "L", "S"] },
-//   { key: -13, n: "Maternal Grandmother", s: "F", m: -31, f: -30, a: ["H", "S"] },
-//   { key: -21, n: "Aunt", s: "F", m: -13, f: -12, a: ["C", "I"] },
-//   { key: -22, n: "Uncle", s: "M", ux: -21 },
-//   { key: -23, n: "Cousin", s: "M", m: -21, f: -22 },
-//   { key: -30, n: "Maternal Great", s: "M", ux: -31, a: ["D", "J", "S"] },
-//   { key: -31, n: "Maternal Great", s: "F", m: -50, f: -51, a: ["B", "H", "L", "S"] },
-//   { key: -42, n: "Great Uncle", s: "M", m: -30, f: -31, a: ["C", "J", "S"] },
-//   { key: -43, n: "Great Aunt", s: "F", m: -30, f: -31, a: ["E", "G", "S"] },
-//   { key: -50, n: "Maternal Great Great", s: "F", vir: -51, a: ["D", "I", "S"] },
-//   { key: -51, n: "Maternal Great Great", s: "M", a: ["B", "H", "S"] }
-// ]
-
-
-// create and initialize the Diagram.model given an array of node data representing people
 
 function setupDiagram(diagram, array, focusId) {
   diagram.model =
